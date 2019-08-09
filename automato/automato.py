075d1c300a517ef74335d8e2077e075db0a7c473
Laboratório 01 <usrlab01@lanfran.local>
2019-08-09 10:26:01 -0300

    construção de automato

---
+++ automato/automato.py
@@ -0,0 +1,16 @@
+
+
+
+
+alfabeto = ['a','b']
+estados = ['Q1','Q2','Q3']
+
+estado_Ini = 'Q1'
+estado_Fin = 'Q2' ## termo reconhecido
+
+regras_tra = [
+            [],
+            []
+            []
+            []
+            ]
